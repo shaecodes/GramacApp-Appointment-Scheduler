250134982961969a172c83c1fff3e3aea5605ce7
Brianna Beharrie <brianna.beharrie@mymona.uwi.edu>
2024-04-05 00:34:47 -0500

    Modified file

--- views/registration_form.php
+++ views/registration_form.php
@@ -6,11 +6,11 @@
 <body>
     <h2>Registration</h2>
     <form method="post" action="../controllers/RegistrationController.php">
-        <label>First Name:</label><input type="text" name="first_name" required><br>
-        <label>Last Name:</label><input type="text" name="last_name" required><br>
-        <label>Phone Number:</label><input type="text" name="phone_number" required><br>
-        <label>Email:</label><input type="email" name="email" required><br>
-        <label>Password:</label><input type="password" name="password" required><br>
+        <label>First Name:</label><input type="text" name="first_name" required><br><br>
+        <label>Last Name:</label><input type="text" name="last_name" required><br><br>
+        <label>Phone Number:</label><input type="text" name="phone_number" required><br><br>
+        <label>Email:</label><input type="email" name="email" required><br><br>
+        <label>Password:</label><input type="password" name="password" required><br><br>
         <input type="submit" value="Register">
     </form>
 </body>
