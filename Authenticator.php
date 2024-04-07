55f32ac7129b6b860cafc00fe8867a5e4a1bca3f
Brianna Beharrie <brianna.beharrie@mymona.uwi.edu>
2024-04-06 20:53:02 -0500

    Modified authenticator

--- Authenticator.php
+++ Authenticator.php
@@ -3,22 +3,36 @@ require_once 'DatabaseController.php';
 
 class Authenticator {
     private $db;
+
+    public function __construct() {
+        $this->db = new DatabaseController();
+    }
+
     public function authenticateUser($email, $password) {
-        // Connect to the database
         $this->db->connect();
         
-        // Query the database to fetch the user with the provided email
         $query = "SELECT * FROM users WHERE email = :email";
-        $stmt = $this->db->getConnection()->prepare($query);
+        $stmt = $this->db->conn->prepare($query);
         $stmt->bindParam(':email', $email);
         $stmt->execute();
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
     
-        // If a user with the provided email exists and the password matches, return true
         if ($user && password_verify($password, $user['password'])) {
             return true;
         } else {
             return false;
         }
     }    
+
+    public function is_admin($email) {
+        $this->db->connect();
+        
+        $query = "SELECT * FROM users WHERE email = :email AND role = 'admin'";
+        $stmt = $this->db->conn->prepare($query);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+        $adminUser = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+        return ($adminUser !== false);
+    }
 }
