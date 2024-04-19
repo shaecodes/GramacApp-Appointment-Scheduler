e749acbba9e0db246fe4e5ea95871830d677b9b4
shaecodes <sinclairroshae@yahoo.com>
2024-04-19 07:47:32 -0500

    Revert "Changed throw exceptions to alerts"
    This reverts commit a724451ca87c760620538c593894de3848d8214b.

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -12,14 +12,13 @@ class LoginController {
     public function loginUser($email, $password) {
         try {
             if (empty($email) || empty($password)) {
-                echo '<script>alert("Email and password are required.");</script>';
-                echo '<script>window.location.href = "../views/login_form.php";</script>'; 
+                throw new Exception("Email and password are required.");
             }
-    
+
             $stmt = $this->db->conn->prepare("SELECT * FROM users WHERE email = :email");
             $stmt->execute([':email' => $email]);
             $user = $stmt->fetch(PDO::FETCH_ASSOC);
-    
+
             if ($user && password_verify($password, $user['password'])) {
                 session_start();
                 $_SESSION['user_id'] = $user['id'];
@@ -28,21 +27,15 @@ class LoginController {
                 header("Location: ../views/appointment_form.php");
                 exit;
             } else {
-                echo '<script>alert("Invalid email or password.");</script>';
-                echo '<script>window.location.href = "../views/login_form.php";</script>'; 
-                exit;
+                throw new Exception("Invalid email or password.");
             }
         } catch(PDOException $e) {
             echo "An error occurred while processing your request. Please try again later.";
             echo "Error: " . $e->getMessage();
         } catch(Exception $e) {
-            echo '<script>alert("' . $e->getMessage() . '");</script>';
-            echo '<script>window.location.href = "../views/login_form.php";</script>'; // Change "login.php" to your login page
-            exit;
+            echo $e->getMessage();
         }
     }
-    
-    
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
