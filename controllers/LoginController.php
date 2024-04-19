a724451ca87c760620538c593894de3848d8214b
shaecodes <sinclairroshae@yahoo.com>
2024-04-19 07:08:01 -0500

    Changed throw exceptions to alerts

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -12,13 +12,14 @@ class LoginController {
     public function loginUser($email, $password) {
         try {
             if (empty($email) || empty($password)) {
-                throw new Exception("Email and password are required.");
+                echo '<script>alert("Email and password are required.");</script>';
+                echo '<script>window.location.href = "../views/login_form.php";</script>'; 
             }
-
+    
             $stmt = $this->db->conn->prepare("SELECT * FROM users WHERE email = :email");
             $stmt->execute([':email' => $email]);
             $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
+    
             if ($user && password_verify($password, $user['password'])) {
                 session_start();
                 $_SESSION['user_id'] = $user['id'];
@@ -26,15 +27,21 @@ class LoginController {
                 header("Location: ../views/appointment_form.php");
                 exit;
             } else {
-                throw new Exception("Invalid email or password.");
+                echo '<script>alert("Invalid email or password.");</script>';
+                echo '<script>window.location.href = "../views/login_form.php";</script>'; 
+                exit;
             }
         } catch(PDOException $e) {
             echo "An error occurred while processing your request. Please try again later.";
             echo "Error: " . $e->getMessage();
         } catch(Exception $e) {
-            echo $e->getMessage();
+            echo '<script>alert("' . $e->getMessage() . '");</script>';
+            echo '<script>window.location.href = "../views/login_form.php";</script>'; // Change "login.php" to your login page
+            exit;
         }
     }
+    
+    
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
